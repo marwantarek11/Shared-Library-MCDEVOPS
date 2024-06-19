819f7d6a2f552c307a52224813b9b94847311330
MarwanTarek <167176241+marwantarek11@users.noreply.github.com>
2024-06-19 06:31:43 +0300

    Update checkRepo.groovy

--- vars/checkRepo.groovy
+++ vars/checkRepo.groovy
@@ -1,5 +1,5 @@
 #!usr/bin/env groovy
 def call() {
 	echo "checking Git Repo..."
-	git branch: 'dev', credentialsId: 'GitHub', url: 'https://github.com/marwantarek11/MultiCloudDevOpsProject.git'
+	git branch: 'Dev', credentialsId: 'GitHub', url: 'https://github.com/marwantarek11/MultiCloudDevOpsProject.git'
 }
